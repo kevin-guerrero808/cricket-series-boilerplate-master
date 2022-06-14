a4585c1912ba006240ce9074ad5815c26ef94220
kevin.guerrero <kevin.guerrero@newshore.es>
2022-06-13 20:54:52 -0500

    add implementation to methods

--- src/PerformanceCalc.js
+++ src/PerformanceCalc.js
@@ -1,6 +1,19 @@
 
 const PerformanceCalculator=(runs,balls)=>{
-      // Write the Logic here
+      if (runs > 100 && balls < 50) {
+            return calculateAverage(runs, balls, 20);
+      } else if (runs > 50 && balls < 20) {
+            return calculateAverage(runs, balls, 10);
+      } else if (runs > 30 && balls < 15) {
+            return calculateAverage(runs, balls, 1);
+      } else {
+            return calculateAverage(runs, balls, 0);
+      }
+}
+
+const calculateAverage = (runs, balls, aditionalPercent) => {
+      const average = runs / balls;
+      return average + (average * aditionalPercent / 100);
 }
 
 module.exports={PerformanceCalculator}
